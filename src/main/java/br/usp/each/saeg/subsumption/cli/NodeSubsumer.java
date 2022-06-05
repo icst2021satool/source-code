f24ae363c6fa354fdb0f663dfe7c30267dde3201
marcoschaim <marcoschaim@gmail.com>
2022-06-05 18:48:36 -0300

    Changes to produce the fault detection ability data.

--- src/main/java/br/usp/each/saeg/subsumption/cli/NodeSubsumer.java
+++ src/main/java/br/usp/each/saeg/subsumption/cli/NodeSubsumer.java
@@ -20,6 +20,7 @@ public class NodeSubsumer {
     public static int nodeSubsumeAll(File src, InputStream input, String path) {
         int n = 0; // # of methods analyzed
         boolean printLocalDuaNodeFile = false;
+        boolean printDuaJsonNodeFile = true;
         try {
             ClassInfo ci = new ClassInfo(input);
             path = path + File.separator;
@@ -47,28 +48,38 @@ public class NodeSubsumer {
                 final TimeWatch tw = TimeWatch.start();
                 SubsumptionAnalyzer duaSubAnalyzer = new SubsumptionAnalyzer(mi.getProgram(), mi.getDuas());
                 Graphdua grd = duaSubAnalyzer.findNode2DuasSubsumption();
+                mi.setGraphDua(grd);
+                mi.setSubsumptionAnalyzer(duaSubAnalyzer);
 
                 final long milliseconds;
+                milliseconds = tw.time(TimeUnit.MILLISECONDS);
                 System.out.println("\n#" + ci.getName() + File.separator + mi.getName() + ":");
 
-                if (printLocalDuaNodeFile) {
+                if (printLocalDuaNodeFile && !printDuaJsonNodeFile) {
                     writeBufferToFile(path, methodname + ".ns", grd.toDotNodeSubsumption(duaSubAnalyzer));
-                    milliseconds = tw.time(TimeUnit.MILLISECONDS);
                 } else {
-                    int noSubmedDuas = grd.getAllDuasSubsumedNode(duaSubAnalyzer).cardinality();
-                    milliseconds = tw.time(TimeUnit.MILLISECONDS);
-                    System.out.println("## nodes: " + mi.getProgram().getGraph().size());
-                    System.out.println("## edges: " + mi.getProgram().getGraph().sizeEdges());
-                    System.out.println("## DUAs: " + mi.getDuas().size());
-                    System.out.println("## Subsumed DUAs: " + noSubmedDuas);
-                    System.out.println("## Node DUA coverage: " + ((double) noSubmedDuas / mi.getDuas().size()) * 100);
-                    System.out.println("@@ " + methodname + "," + mi.getProgram().getGraph().size() + "," + mi.getProgram().getGraph().sizeEdges() + "," + mi.getDuas().size() + "," + noSubmedDuas + "," + ((double) noSubmedDuas / mi.getDuas().size()) * 100 + "," + milliseconds / 1000 + "," + milliseconds + "\n");
+                    if (!printLocalDuaNodeFile && !printDuaJsonNodeFile) {
+                        int noSubmedDuas = grd.getAllDuasSubsumedNode(duaSubAnalyzer).cardinality();
+//                        milliseconds = tw.time(TimeUnit.MILLISECONDS);
+                        System.out.println("## nodes: " + mi.getProgram().getGraph().size());
+                        System.out.println("## edges: " + mi.getProgram().getGraph().sizeEdges());
+                        System.out.println("## DUAs: " + mi.getDuas().size());
+                        System.out.println("## Subsumed DUAs: " + noSubmedDuas);
+                        System.out.println("## Node DUA coverage: " + ((double) noSubmedDuas / mi.getDuas().size()) * 100);
+                        System.out.println("@@ " + methodname + "," + mi.getProgram().getGraph().size() + "," + mi.getProgram().getGraph().sizeEdges() + "," + mi.getDuas().size() + "," + noSubmedDuas + "," + ((double) noSubmedDuas / mi.getDuas().size()) * 100 + "," + milliseconds / 1000 + "," + milliseconds + "\n");
+                    }
                 }
 
                 System.out.println("\n#" + ci.getName() + File.separator + mi.getName() + ":");
                 System.out.println(MessageFormat.format(
                         "Local DUA-Node subsumption of method {0} calculated in {1} minutes, {2} seconds, and {3} milliseconds", methodname, (milliseconds / 1000) / 60, (milliseconds / 1000) % 60, milliseconds));
                 n++;
+            }
+            if (printDuaJsonNodeFile) {
+                
+                writeBufferToFile(path, ci.getName().replace(File.separator, ".") + ".nodes.json", ci.toJsonNodes());
+                writeBufferToFile(path, ci.getName().replace(File.separator, ".") + ".duas2nodes.json", ci.toJsonDuas2Nodes());
+                writeBufferToFile(path, ci.getName().replace(File.separator, ".") + ".nodesub.json", ci.toJsonNodeSubsumption());
             }
         } catch (Exception e) {
             String failfile = src.getPath();
